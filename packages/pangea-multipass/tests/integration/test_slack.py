1d905ff94b6f1ab94fed6d2f9bbc1f38e434b35d
H. Andres Tournour <andres.tournour@pangea.cloud>
2025-02-24 16:03:09 -0300

    SlackReader pagination support and tests

--- packages/pangea-multipass/tests/integration/test_slack.py
+++ packages/pangea-multipass/tests/integration/test_slack.py
@@ -8,6 +8,7 @@ user_email = os.getenv("SLACK_USER_EMAIL") or ""
 
 _TOTAL_FILES = 12
 _AUTHORIZED_FILES = 7
+_TOTAL_CHANNELS = 4
 _AUTHORIZED_CHANNELS = 3
 
 
@@ -17,7 +18,7 @@ class TestSlack(unittest.TestCase):
         assert user_email
 
     def test_slack(self) -> None:
-        reader = SlackReader(token=token, max_messages=2)
+        reader = SlackReader(token=token)
         documents = reader.load_data()
         assert len(documents) == _TOTAL_FILES
 
@@ -27,3 +28,21 @@ class TestSlack(unittest.TestCase):
 
         filtered_docs = processor.filter(nodes=documents)
         assert len(filtered_docs) == _AUTHORIZED_FILES
+
+    def test_slack_pagination(self) -> None:
+        reader = SlackReader(token=token)
+
+        channels = reader.get_channels()
+        assert len(channels) == _TOTAL_CHANNELS
+
+        documents = []
+
+        for channel in channels:
+            has_more_messages = True
+            while has_more_messages:
+                docs = reader.read_messages(channel=channel, page_size=1)
+                assert len(docs) <= 1  # Some messages are filtered out so we can't guarantee the exact number
+                documents.extend(docs)
+                has_more_messages = reader.has_more_messages
+
+        assert len(documents) == _TOTAL_FILES
